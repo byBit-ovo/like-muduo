b7b71e8fad579b3e9132a04d436daaf62672b2de
xunyi <3302371508@qq.com>
2025-06-14 21:18:31 +0800

    test of alive connecton and release

--- test/client.cpp
+++ test/client.cpp
@@ -14,16 +14,25 @@ int main(int argc,char* argv[])
     client.BuildClient(ip, port);
     char in[1024];
     std::string message = "GET /hello HTTP/1.1\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n";
-    while (1)
+    int n = 10;
+    while (n--)
     {
         int ret = client.Send(message.c_str(), message.size(), false);
         if(ret<0){
             exit(0);
         }
-        sleep(3);
+        sleep(1);
         int n = client.Read(in, 1023, true);
-        in[n] = 0;
+        in[n] = 0; 
         std::cout << in << std::endl;
+    }
+    std::cout << "sleeping..." << std::endl;
+    int t = 10;
+    while (1)
+    {
+        std::cout << t << std::endl;
+        t--;
+        sleep(1);
     }
     client.Close();
     return 0;
